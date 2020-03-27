20ee55afe4e7e0e138783b475fb13a714dfca662
David Roach <David.Roach@siriusxm.com>
2020-03-27 17:54:38 -0400

    updating commit
    Signed-off-by: David Roach <David.Roach@siriusxm.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `david-roach-personal-web`,
+        short_name: `droach`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#32C5FF`,
+        theme_color: `#32C5FF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/roach-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
